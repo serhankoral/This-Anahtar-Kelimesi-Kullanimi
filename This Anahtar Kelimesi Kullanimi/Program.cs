bd71b9f17183c563635f863b1cf0424bcdf1aaa9
Serhan KORAL <serhan_koral@hotmail.com>
2023-01-18 23:17:58 +0300

    this ana class ı değerlerine ulaştırır

--- This Anahtar Kelimesi Kullanimi/Program.cs
+++ This Anahtar Kelimesi Kullanimi/Program.cs
@@ -2,11 +2,36 @@
 
 namespace This_Anahtar_Kelimesi_Kullanimi
 {
-    internal class Program
+    class Program
     {
+        public int x;
         static void Main(string[] args)
         {
-           
+           Program program =new Program();
+        }
+        public Program() 
+        {
+            A nesne = new A(5, 15, 25);
+            Console.WriteLine(nesne.x);
+            Console.WriteLine(nesne.y);
+            Console.WriteLine(nesne.z);
         }
     }
+
+    class A
+    {
+        public  int x;
+        public  int y;
+        public  int z;
+        private A(int x)
+        {
+            this.x = x;
+        }
+        public A(int x, int y, int z) : this(x)
+        {
+            this.y = y;
+            this.z = z;
+        }
+    }
+
 }
