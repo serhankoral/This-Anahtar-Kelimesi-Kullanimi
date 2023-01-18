1a10d534b97fc37be8c8227c47a9600764d18246
Serhan KORAL <serhan_koral@hotmail.com>
2023-01-18 23:07:36 +0300

    Proje dosyası ekle.

---
+++ This Anahtar Kelimesi Kullanimi/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace This_Anahtar_Kelimesi_Kullanimi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+        }
+    }
+}
